005fb9c626d257fafd03d3b79a9f812e0c80d2fd
jigoo <165612433+jisu0212@users.noreply.github.com>
2024-07-07 17:40:05 +0900

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,23 @@ window.addEventListener('scroll',function(){
         head.style.transition = 'background-color 0.3s'
     }
 })
+//=================================tab
+const categoryBtn = document.querySelectorAll ('.part4_tab a')
+const contents = document.querySelectorAll ('.best .part4_contents')
+for(let i = 0; i<7; i++){
+    categoryBtn[i].addEventListener('click', function(e){
+        e.preventDefault()
+        contents[0].style = 'display:none';
+        contents[1].style = 'display:none';
+        contents[2].style = 'display:none';
+        contents[3].style = 'display:none';
+        contents[4].style = 'display:none';
+        contents[i].style = 'display:flex';
+        categoryBtn[0].style = 'text-decoration: none; font-weight:400';
+        categoryBtn[1].style = 'text-decoration: none; font-weight:400';
+        categoryBtn[2].style = 'text-decoration: none; font-weight:400';
+        categoryBtn[3].style = 'text-decoration: none; font-weight:400';
+        categoryBtn[4].style = 'text-decoration: none; font-weight:400';
+        categoryBtn[i].style = 'text-decoration: underline; font-weight:600';
+    })
+}
